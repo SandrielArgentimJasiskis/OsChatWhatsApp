d2e55b0dac609f05577037b637c054813ba8568c
SandrielArgentimJasiskis <78165452+SandrielArgentimJasiskis@users.noreply.github.com>
2024-10-11 12:29:09 -0300

    Add files via upload

---
+++ config.php
@@ -0,0 +1,18 @@
+<?php
+
+    // URL.
+    define('URL_APP', '');
+
+    // DIR.
+    define('DIR_MEDIA', __DIR__ . '/media/');
+    define('DIR_APP', __DIR__ . '/app/');
+    define('DIR_SYSTEM', __DIR__ . '/system/');
+    
+    // DATABASE.
+    define('DB_NAME', '');
+    define('DB_USER', '');
+    define('DB_PASS', '');
+    define('DB_PREFIX', 'bot_');
+    define('DB_CHARSET', 'utf8');
+    define('DB_HOST', 'localhost');
+    
